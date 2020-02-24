180ab45db68af5caf76edcd8e5e787ace09f2625
Aatif Jiwani <aatif.jiwani@gmail.com>
2020-02-23 16:29:09 -0800

    finished 3, q4

---
+++ hw3/question3.py
@@ -0,0 +1,85 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+def multivariate_gaussian(X, mu, sigma):
+    d = mu.shape[0]
+    det = np.linalg.det(sigma)
+    denominator = (2*np.pi)**(d/2) * np.sqrt(det)
+    exponent = -0.5 * (X - mu).T.dot(np.linalg.inv(sigma)).dot((X - mu))
+    return float( (1/denominator) * np.exp(exponent) )
+
+def generateZ(mu, sigma):
+    Z = np.empty((150,150))
+    for i in range(0, 150):
+        for j in range(0, 150):
+            Z[i,j] = multivariate_gaussian(plot[i,j,:].reshape(2,1), mu, sigma)
+
+    return Z
+
+def plotGaussian(X, Y, Z):
+    plt.contourf(X, Y, Z, 20, cmap="viridis")
+    plt.colorbar()
+    plt.show()
+    plt.waitforbuttonpress()
+    ## Save Plot 
+
+plot = np.empty((150,150,2))
+X,Y = np.meshgrid(np.linspace(-5, 5, 150), np.linspace(-5, 5, 150))
+plot[:,:,0] = X
+plot[:,:,1] = Y
+
+## 3.1
+def q3_1():
+    mu = np.array([1,1]).reshape(2,1)
+    sigma = np.array([1,0,0,2]).reshape(2,2)
+    Z = generateZ(mu, sigma)
+    plotGaussian(X,Y,Z)
+
+def q3_2():
+    mu = np.array([-1,2]).reshape(2,1)
+    sigma = np.array([2,1,1,4]).reshape(2,2)
+    Z = generateZ(mu, sigma)
+    plotGaussian(X,Y,Z)
+
+def q3_3():
+    mu1 = np.array([0,2]).reshape(2,1)
+    mu2 = np.array([2,0]).reshape(2,1)
+
+    sigma = np.array([2,1,1,1]).reshape(2,2)
+
+    Z1 = generateZ(mu1, sigma)
+    Z2 = generateZ(mu2, sigma)
+
+    plotGaussian(X,Y,Z1 - Z2)
+
+def q3_4():
+    mu1 = np.array([0,2]).reshape(2,1)
+    mu2 = np.array([2,0]).reshape(2,1)
+
+    sigma1 = np.array([2,1,1,1]).reshape(2,2)
+    sigma2 = np.array([2,1,1,4]).reshape(2,2)
+
+    Z1 = generateZ(mu1, sigma1)
+    Z2 = generateZ(mu2, sigma2)
+
+    plotGaussian(X,Y,Z1 - Z2)
+
+
+def q3_5():
+    mu1 = np.array([1,1]).reshape(2,1)
+    mu2 = np.array([-1,-1]).reshape(2,1)
+
+    sigma1 = np.array([2,0,0,1]).reshape(2,2)
+    sigma2 = np.array([2,1,1,2]).reshape(2,2)
+
+    Z1 = generateZ(mu1, sigma1)
+    Z2 = generateZ(mu2, sigma2)
+
+    plotGaussian(X,Y,Z1 - Z2)
+
+# q3_1()
+# q3_2()
+# q3_3()
+q3_4()
+q3_5()
+    
---
+++ hw3/question4.py
@@ -0,0 +1,64 @@
+import matplotlib.pyplot as plt
+import numpy as np
+from scipy import stats
+
+
+np.random.seed(7)
+
+def generateRandomSample(num_samples):
+    n = np.zeros((num_samples, 2)).astype(np.float32)
+    X1 = np.random.normal(loc=3, scale=3, size=100)
+    X2 = 0.5*X1 + np.random.normal(loc=4, scale=2, size=100)
+
+    n[:, 0] = X1
+    n[:, 1] = X2
+
+    return n
+
+
+n = generateRandomSample(100)
+print()
+
+mean_sample = np.mean(n, axis=0)
+print("-- Mean of Sample --")
+print(mean_sample, end='\n\n')
+
+cov_matrix = np.cov(n.T)
+print("-- Covariance Matrix --")
+print(cov_matrix, end='\n\n')
+
+eigenvalues, eigenvectors = np.linalg.eig(cov_matrix)
+print("-- Eigenvector V1 with associated Lambda 1 --")
+print("V1:", eigenvectors[:,0], "with eigenvalue", eigenvalues[0])
+print()
+print("-- Eigenvector V2 with associated Lambda 2 --")
+print("V2:", eigenvectors[:,1], "with eigenvalue", eigenvalues[1])
+
+# plt.scatter(n[:,0], n[:,1], color='darkblue', s=6)
+# plt.arrow(*mean_sample, *(eigenvalues[1]*eigenvectors[:,1]), color='darkred', width=0.1)
+# plt.arrow(*mean_sample, *(eigenvalues[0]*eigenvectors[:,0]), color='green', width=0.1)
+# plt.xlim([-15, 15])
+# plt.ylim([-15, 15])
+# plt.grid()
+# plt.axes().set_aspect('equal')
+# plt.show()
+# plt.waitforbuttonpress()
+
+maxEigenvector = np.argmax(eigenvalues)
+v1 = eigenvectors[:,maxEigenvector].reshape(2,1)
+v2 = eigenvectors[:,1 - maxEigenvector].reshape(2,1)
+
+U = np.concatenate((v1,v2), axis=1)
+centered_n = n - mean_sample
+rotated_n = centered_n.dot(U)
+
+plt.scatter(rotated_n[:,0], rotated_n[:,1], color='darkblue', s=6)
+#plt.arrow(*mean_sample, *(eigenvalues[1]*eigenvectors[:,1]), color='darkred', width=0.1)
+#plt.arrow(*mean_sample, *(eigenvalues[0]*eigenvectors[:,0]), color='green', width=0.1)
+plt.xlim([-15, 15])
+plt.ylim([-15, 15])
+plt.grid()
+plt.axes().set_aspect('equal')
+plt.show()
+plt.waitforbuttonpress()
+
